4300974e2e204c226eb281c32c5c2607be1fd993
Erich Uher <ejuher@gmail.com>
2019-01-20 13:42:11 -0800

    order the scopes by date

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,4 +1,4 @@
 class Gig < ApplicationRecord
-  scope :upcomings, -> { where('date >= ?', Date.current) }
-  scope :priors, -> { where('date < ?', Date.current) }
+  scope :upcomings, -> { where('date >= ?', Date.current).order(date: :desc) }
+  scope :priors, -> { where('date < ?', Date.current).order(date: :desc) }
 end
