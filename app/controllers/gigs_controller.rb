2a14013b3467cebaf3f3c279de8a3af2add64a8b
Erich Uher <ejuher@gmail.com>
2019-01-19 21:42:57 -0800

    create gigs index seperating upcoming and past gigs

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -2,3 +2,12 @@ class GigsController < ApplicationController
   def index
   end
 end
+
+# page for viewing all gigs
+  # upcoming is above a line, past is below a line
+
+# page for new gig
+  # doing autocomplete, or picking from a dropdown would be cool for cities and venues
+
+# page for editing an existing gig
+  # here there is a link/button for deleting the gig. Will go to the delete method which will perform the delete then redirect to indesx
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,2 +1,4 @@
 class Gig < ApplicationRecord
+  scope :upcomings, -> { where('date >= ?', Date.current) }
+  scope :priors, -> { where('date < ?', Date.current) }
 end
