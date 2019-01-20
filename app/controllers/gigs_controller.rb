fc28dc870fc7d9897600a65c4fbe6341f2a7d05c
Erich Uher <ejuher@gmail.com>
2019-01-20 13:42:52 -0800

    create api route/method for upcoming gigs

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,6 +1,10 @@
 class GigsController < ApplicationController
   def index
   end
+
+  def upcoming
+    render json: Gig.upcomings
+  end
 end
 
 # page for viewing all gigs
@@ -11,3 +15,8 @@ end
 
 # page for editing an existing gig
   # here there is a link/button for deleting the gig. Will go to the delete method which will perform the delete then redirect to indesx
+
+# api route for api requests!
+  # api/gig (or is it gigs?)
+  # accepts param for how to scope
+  # or api/upcoming_gigs 
